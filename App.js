057d450f91749cea14061188eaa90feb1ae95185
Atkenny <tellezkenny08@gmail.com>
2024-10-17 08:27:29 -0600

    Implementando Grafico de progreso GraficoProgreso.js y anexando a App.js

--- App.js
+++ App.js
@@ -1,52 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import GraficoSalarios from './src/components/GraficoSalarios';
-import GraficoGeneros from './src/components/GraficoGeneros';
-import Formulario from './src/components/Formulario';
-import GraficoReporteEnfermedades from './src/components/GraficoReporteEnfermedades';
-import GraficoBezier from './src/components/GraficoBezier';
-import { collection, getDocs, query } from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import GraficoSalarios from "./src/components/GraficoSalarios";
+import GraficoGeneros from "./src/components/GraficoGeneros";
+import Formulario from "./src/components/Formulario";
+import GraficoReporteEnfermedades from "./src/components/GraficoReporteEnfermedades";
+import GraficoBezier from "./src/components/GraficoBezier";
+import { collection, getDocs, query } from "firebase/firestore";
+import GraficoProgreso from "./src/components/GraficoProgreso";
 
 //Importación de conexión a firebase
-import db from './database/firebaseConfig';
-
+import db from "./database/firebaseConfig";
 
 export default function Graficos() {
+  const [dataProgreso, setDataProgreso] = useState({
+    labels: [""],
+    data: [0],
+  });
 
   const [bandera, setBandera] = useState(false); // Variable bandera
   const [dataSalarios, setDataSalarios] = useState({
-    labels: [''],
-    datasets: [{ data: [0] }]
+    labels: [""],
+    datasets: [{ data: [0] }],
   });
   const [dataGeneros, setDataGeneros] = useState([]); // Para almacenar datos de géneros
 
   const dataReporteEnfermedades = [
-    { date: "2017-01-05", count: 8 }, 
-    { date: "2017-01-19", count: 5 }, 
-    { date: "2017-02-06", count: 2 }, 
-    { date: "2017-02-20", count: 4 }, 
-    { date: "2017-03-07", count: 1 }, 
-    { date: "2017-03-21", count: 3 }, 
-    { date: "2017-04-05", count: 6 }, 
-    { date: "2017-04-19", count: 2 }, 
+    { date: "2017-01-05", count: 8 },
+    { date: "2017-01-19", count: 5 },
+    { date: "2017-02-06", count: 2 },
+    { date: "2017-02-20", count: 4 },
+    { date: "2017-03-07", count: 1 },
+    { date: "2017-03-21", count: 3 },
+    { date: "2017-04-05", count: 6 },
+    { date: "2017-04-19", count: 2 },
     { date: "2017-05-03", count: 4 },
     { date: "2017-05-17", count: 7 },
-    { date: "2017-06-06", count: 9 }, 
-    { date: "2017-06-20", count: 5 }, 
-    { date: "2017-07-05", count: 3 }, 
-    { date: "2017-07-19", count: 4 }, 
-    { date: "2017-08-07", count: 2 },  
-    { date: "2017-08-21", count: 8 },  
+    { date: "2017-06-06", count: 9 },
+    { date: "2017-06-20", count: 5 },
+    { date: "2017-07-05", count: 3 },
+    { date: "2017-07-19", count: 4 },
+    { date: "2017-08-07", count: 2 },
+    { date: "2017-08-21", count: 8 },
     { date: "2017-09-06", count: 3 },
     { date: "2017-09-20", count: 7 },
     { date: "2017-10-04", count: 5 },
     { date: "2017-10-18", count: 6 },
     { date: "2017-11-06", count: 2 },
-    { date: "2017-11-20", count: 9 }, 
+    { date: "2017-11-20", count: 9 },
     { date: "2017-12-05", count: 4 },
-    { date: "2017-12-19", count: 7 } 
+    { date: "2017-12-19", count: 7 },
   ];
-  
+
   // Carga de datos de salarios
   useEffect(() => {
     const recibirDatosSalarios = async () => {
@@ -59,14 +63,14 @@ export default function Graficos() {
         querySnapshot.forEach((doc) => {
           const datosBD = doc.data();
           const { nombre, salario } = datosBD;
-            nombres.push(nombre); // Agrega nombre a la lista
-            salarios.push(salario); // Agrega edad a la lista
+          nombres.push(nombre); // Agrega nombre a la lista
+          salarios.push(salario); // Agrega edad a la lista
         });
 
         // Actualiza el estado con el formato requerido
         setDataSalarios({
           labels: nombres,
-          datasets: [{ data: salarios }]
+          datasets: [{ data: salarios }],
         });
 
         console.log({ labels: nombres, datasets: [{ data: salarios }] });
@@ -103,18 +107,27 @@ export default function Graficos() {
           {
             name: "Masculino",
             population: masculino,
-            color: "rgba(131, 167, 234, 0.5)",  // Azul con 50% de intensidad
+            color: "rgba(131, 167, 234, 0.5)", // Azul con 50% de intensidad
             legendFontColor: "#7F7F7F",
-            legendFontSize: 12
+            legendFontSize: 12,
           },
           {
             name: "Femenino",
             population: femenino,
-            color: "rgba(255, 105, 180, 0.5)",  // Rosa con 50% de intensidad
+            color: "rgba(255, 105, 180, 0.5)", // Rosa con 50% de intensidad
             legendFontColor: "#7F7F7F",
-            legendFontSize: 12
-          }
+            legendFontSize: 12,
+          },
         ];
+
+        totalPersonas = masculino + femenino;
+
+        const progresos = [masculino / totalPersonas, femenino / totalPersonas];
+
+        setDataProgreso({
+          labels: ["Hombres", "Mujeres"],
+          data: progresos,
+        });
 
         setDataGeneros(totalData);
         console.log(totalData);
@@ -127,24 +140,28 @@ export default function Graficos() {
   }, [bandera]);
 
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
-        <Formulario setBandera={setBandera}/>
-        <GraficoSalarios dataSalarios={dataSalarios}/>
-        <GraficoBezier dataSalarios={dataSalarios}/>
-        <GraficoGeneros dataGeneros={dataGeneros}/>
-        <GraficoReporteEnfermedades dataReporteEnfermedades={dataReporteEnfermedades}/>
+        <Formulario setBandera={setBandera} />
+        <GraficoProgreso
+          dataProgreso={dataProgreso}
+          colors={["rgba(131, 167, 234, 0.5)", "rgba(255, 105, 180, 0.5)"]}
+        />
+        <GraficoSalarios dataSalarios={dataSalarios} />
+        <GraficoBezier dataSalarios={dataSalarios} />
+        <GraficoGeneros dataGeneros={dataGeneros} />
+        <GraficoReporteEnfermedades
+          dataReporteEnfermedades={dataReporteEnfermedades}
+        />
       </ScrollView>
-
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   scrollView: {
     padding: 10,
@@ -153,4 +170,4 @@ const styles = StyleSheet.create({
     marginTop: 10,
     padding: 10,
   },
-}); 
+});
