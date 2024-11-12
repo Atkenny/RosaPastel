8ccc9397e40f3cbffec57a7b91f7dbf95c8cd55c
Atkenny <tellezkenny08@gmail.com>
2024-11-12 08:27:47 -0600

    Implementacion de Grafico con filtro de Ventas, Proyectos y Servicios.

--- src/components/Graficos.js
+++ src/components/Graficos.js
@@ -1,59 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import GraficoSalarios from './GraficoSalarios';
-import GraficoGeneros from './GraficoGeneros';
-import Formulario from './Formulario';
-import GraficoReporteEnfermedades from './GraficoReporteEnfermedades';
-import GraficoBezier from './GraficoBezier';
-import GraficoProgreso from './GraficoProgreso';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import GraficoSalarios from "./GraficoSalarios";
+import GraficoGeneros from "./GraficoGeneros";
+import Formulario from "./Formulario";
+import GraficoReporteEnfermedades from "./GraficoReporteEnfermedades";
+import GraficoBezier from "./GraficoBezier";
+import GraficoProgreso from "./GraficoProgreso";
+import GraficoFiltrado from "./GraficoFiltrado";
 
-import { collection, getDocs, query } from 'firebase/firestore';
-
+import { collection, getDocs, query } from "firebase/firestore";
 
 //Importación de conexión a firebase
-import db from '../database/firebaseconfig';
+import db from "../database/firebaseconfig";
 
 export default function Graficos() {
-
   const [bandera, setBandera] = useState(false); // Variable bandera
   const [dataSalarios, setDataSalarios] = useState({
-    labels: [''],
-    datasets: [{ data: [0] }]
+    labels: [""],
+    datasets: [{ data: [0] }],
   });
   const [dataGeneros, setDataGeneros] = useState([]); // Para almacenar datos de géneros
 
   const [dataProgreso, setDataProgreso] = useState({
-    labels: [''],
-    data: [0]
+    labels: [""],
+    data: [0],
   });
 
   const dataReporteEnfermedades = [
-    { date: "2017-01-05", count: 8 }, 
-    { date: "2017-01-19", count: 5 }, 
-    { date: "2017-02-06", count: 2 }, 
-    { date: "2017-02-20", count: 4 }, 
-    { date: "2017-03-07", count: 1 }, 
-    { date: "2017-03-21", count: 3 }, 
-    { date: "2017-04-05", count: 6 }, 
-    { date: "2017-04-19", count: 2 }, 
+    { date: "2017-01-05", count: 8 },
+    { date: "2017-01-19", count: 5 },
+    { date: "2017-02-06", count: 2 },
+    { date: "2017-02-20", count: 4 },
+    { date: "2017-03-07", count: 1 },
+    { date: "2017-03-21", count: 3 },
+    { date: "2017-04-05", count: 6 },
+    { date: "2017-04-19", count: 2 },
     { date: "2017-05-03", count: 4 },
     { date: "2017-05-17", count: 7 },
-    { date: "2017-06-06", count: 9 }, 
-    { date: "2017-06-20", count: 5 }, 
-    { date: "2017-07-05", count: 3 }, 
-    { date: "2017-07-19", count: 4 }, 
-    { date: "2017-08-07", count: 2 },  
-    { date: "2017-08-21", count: 8 },  
+    { date: "2017-06-06", count: 9 },
+    { date: "2017-06-20", count: 5 },
+    { date: "2017-07-05", count: 3 },
+    { date: "2017-07-19", count: 4 },
+    { date: "2017-08-07", count: 2 },
+    { date: "2017-08-21", count: 8 },
     { date: "2017-09-06", count: 3 },
     { date: "2017-09-20", count: 7 },
     { date: "2017-10-04", count: 5 },
     { date: "2017-10-18", count: 6 },
     { date: "2017-11-06", count: 2 },
-    { date: "2017-11-20", count: 9 }, 
+    { date: "2017-11-20", count: 9 },
     { date: "2017-12-05", count: 4 },
-    { date: "2017-12-19", count: 7 } 
+    { date: "2017-12-19", count: 7 },
   ];
-  
+
   // Carga de datos de salarios
   useEffect(() => {
     const recibirDatosSalarios = async () => {
@@ -66,14 +65,14 @@ export default function Graficos() {
         querySnapshot.forEach((doc) => {
           const datosBD = doc.data();
           const { nombre, salario } = datosBD;
-            nombres.push(nombre); // Agrega nombre a la lista
-            salarios.push(salario); // Agrega edad a la lista
+          nombres.push(nombre); // Agrega nombre a la lista
+          salarios.push(salario); // Agrega edad a la lista
         });
-        
+
         // Actualiza el estado con el formato requerido
         setDataSalarios({
           labels: nombres,
-          datasets: [{ data: salarios }]
+          datasets: [{ data: salarios }],
         });
 
         console.log({ labels: nombres, datasets: [{ data: salarios }] });
@@ -110,26 +109,26 @@ export default function Graficos() {
           {
             name: "Masculino",
             population: masculino,
-            color: "rgba(131, 167, 234, 0.5)",  // Azul con 50% de intensidad
+            color: "rgba(131, 167, 234, 0.5)", // Azul con 50% de intensidad
             legendFontColor: "#7F7F7F",
-            legendFontSize: 12
+            legendFontSize: 12,
           },
           {
             name: "Femenino",
             population: femenino,
-            color: "rgba(255, 105, 180, 0.5)",  // Rosa con 50% de intensidad
+            color: "rgba(255, 105, 180, 0.5)", // Rosa con 50% de intensidad
             legendFontColor: "#7F7F7F",
-            legendFontSize: 12
-          }
+            legendFontSize: 12,
+          },
         ];
-          
+
         totalPersonas = masculino + femenino;
 
-        const progresos = [masculino/totalPersonas, femenino/totalPersonas]
+        const progresos = [masculino / totalPersonas, femenino / totalPersonas];
 
         setDataProgreso({
-          labels: ['Hombres', 'Mujeres'],
-          data: progresos
+          labels: ["Hombres", "Mujeres"],
+          data: progresos,
         });
 
         setDataGeneros(totalData);
@@ -142,31 +141,193 @@ export default function Graficos() {
     recibirDatosGeneros();
   }, [bandera]);
 
-  return (
-    <View style={styles.container} >
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <Formulario setBandera={setBandera}/> 
-        <GraficoSalarios dataSalarios={dataSalarios}/>
-        <GraficoBezier dataSalarios={dataSalarios}/>
+  const [datosFiltrados, setDatosFiltrados] = useState({
+    labels: [],
+    datasets: [{ data: [] }],
+  });
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("Ventas"); // Valor por defecto
 
-        <GraficoProgreso 
+  const registros = [
+    {
+      fecha: "2018-01-15",
+      categoria: "Ventas",
+      valor: 450,
+      monto: 11250,
+      responsable: "Juan Pérez",
+    },
+    {
+      fecha: "2018-01-15",
+      categoria: "Ventas",
+      valor: 450,
+      monto: 11250,
+      responsable: "Ana Gómez",
+    },
+    {
+      fecha: "2018-06-20",
+      categoria: "Servicios",
+      valor: 520,
+      monto: 13000,
+      responsable: "Ana Gómez",
+    },
+    {
+      fecha: "2019-03-12",
+      categoria: "Proyectos",
+      valor: 700,
+      monto: 35000,
+      responsable: "Laura Sánchez",
+    },
+    {
+      fecha: "2019-08-25",
+      categoria: "Ventas",
+      valor: 800,
+      monto: 20000,
+      responsable: "Carlos Martínez",
+    },
+    {
+      fecha: "2020-02-10",
+      categoria: "Servicios",
+      valor: 600,
+      monto: 15000,
+      responsable: "David Ramírez",
+    },
+    {
+      fecha: "2020-09-05",
+      categoria: "Proyectos",
+      valor: 950,
+      monto: 47500,
+      responsable: "Marta López",
+    },
+    {
+      fecha: "2021-01-18",
+      categoria: "Ventas",
+      valor: 1100,
+      monto: 27500,
+      responsable: "Andrés Torres",
+    },
+    {
+      fecha: "2021-07-29",
+      categoria: "Servicios",
+      valor: 650,
+      monto: 16250,
+      responsable: "Elena Moreno",
+    },
+    {
+      fecha: "2022-03-14",
+      categoria: "Proyectos",
+      valor: 980,
+      monto: 49000,
+      responsable: "Laura Sánchez",
+    },
+    {
+      fecha: "2022-11-22",
+      categoria: "Ventas",
+      valor: 1200,
+      monto: 30000,
+      responsable: "Juan Pérez",
+    },
+    {
+      fecha: "2023-04-09",
+      categoria: "Servicios",
+      valor: 700,
+      monto: 17500,
+      responsable: "Ana Gómez",
+    },
+    {
+      fecha: "2023-10-15",
+      categoria: "Proyectos",
+      valor: 1050,
+      monto: 52500,
+      responsable: "Carlos Martínez",
+    },
+    {
+      fecha: "2024-02-01",
+      categoria: "Ventas",
+      valor: 1300,
+      monto: 32500,
+      responsable: "David Ramírez",
+    },
+    {
+      fecha: "2024-06-18",
+      categoria: "Servicios",
+      valor: 750,
+      monto: 18750,
+      responsable: "Marta López",
+    },
+    {
+      fecha: "2024-11-05",
+      categoria: "Proyectos",
+      valor: 1150,
+      monto: 57500,
+      responsable: "Elena Moreno",
+    },
+  ];
+
+  // Filtrar y agrupar por fecha para una categoría seleccionada
+  const filtrarMontoPorFechaYCategoria = (categoria) => {
+    // Filtrar registros por la categoría seleccionada
+    const registrosFiltrados = registros.filter(
+      (item) => item.categoria === categoria
+    );
+
+    // Agrupar los registros filtrados por fecha y calcular el monto total por fecha
+    const datosAgrupados = registrosFiltrados.reduce(
+      (acumulado, actual) => {
+        const indice = acumulado.labels.indexOf(actual.fecha);
+
+        if (indice > -1) {
+          // Si la fecha ya existe, suma el monto al valor existente en esa fecha
+          acumulado.datasets[0].data[indice] += actual.monto;
+        } else {
+          // Si no existe, añade la fecha y el monto
+          acumulado.labels.push(actual.fecha);
+          acumulado.datasets[0].data.push(actual.monto);
+        }
+
+        return acumulado;
+      },
+      { labels: [], datasets: [{ data: [] }] }
+    );
+
+    setDatosFiltrados(datosAgrupados);
+  };
+
+  useEffect(() => {
+    // Filtrar por la categoría seleccionada al cargar el componente
+    filtrarMontoPorFechaYCategoria(categoriaSeleccionada);
+  }, [categoriaSeleccionada]);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Formulario setBandera={setBandera} />
+        <GraficoFiltrado
+          datosFiltrados={datosFiltrados}
+          categoriaSeleccionada={categoriaSeleccionada}
+          setCategoriaSeleccionada={setCategoriaSeleccionada}
+          filtrarMontoPorFechaYCategoria={filtrarMontoPorFechaYCategoria}
+        />
+        <GraficoSalarios dataSalarios={dataSalarios} />
+        <GraficoBezier dataSalarios={dataSalarios} />
+
+        <GraficoProgreso
           dataProgreso={dataProgreso}
-          colors={['rgba(131, 167, 234, 0.5)', 'rgba(255, 105, 180, 0.5)']}   
+          colors={["rgba(131, 167, 234, 0.5)", "rgba(255, 105, 180, 0.5)"]}
         />
 
-        <GraficoGeneros dataGeneros={dataGeneros}/>
-        <GraficoReporteEnfermedades dataReporteEnfermedades={dataReporteEnfermedades}/>
+        <GraficoGeneros dataGeneros={dataGeneros} />
+        <GraficoReporteEnfermedades
+          dataReporteEnfermedades={dataReporteEnfermedades}
+        />
+        
       </ScrollView>
-
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   scrollView: {
     padding: 10,
